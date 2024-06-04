9c21cdd8d1ee2c4fcf5c1e01912c43e00dcc297e
Omar Reda <omar.reda@5d-agency.com>
2024-06-04 11:20:41 +0300

    almousa static

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector(".navbar");
+    if (window.scrollY >= 100) {
+        navbar.classList.add("fixed-top");
+        navbar.classList.remove("bg-nav");
+        
+
+    } else {
+        navbar.classList.remove("fixed-top");
+        navbar.classList.add("bg-nav");
+
+    }
+});
+ 
