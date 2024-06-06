80140c20e9a0be0eebb4aa4738e0e5cfa960e617
Omar Reda <omar.reda@5d-agency.com>
2024-06-06 15:23:56 +0300

    -finish of home page

--- js/main.js
+++ js/main.js
@@ -40,6 +40,103 @@ $('.programs-carousel').owlCarousel({
         } 
     }
 })
+$('.admission-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    rtl:true,
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        991:{
+            items:1
+        } ,
+        1200:{
+            items:2
+        } 
+    }
+})
+$('.recent-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    rtl:true,
+    autoplay:true,
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        991:{
+            items:3
+        } ,
+        1200:{
+            items:3
+        } 
+    }
+});
+$('.meet-our-people-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    rtl:true,
+    autoplay:true,
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        991:{
+            items:3
+        } ,
+        1200:{
+            items:3
+        } 
+    }
+});
+$('.stories-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    rtl:true,
+    autoplay:true,
+    navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"], // Replace navigation text with Font Awesome icons
+
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        991:{
+            items:1
+        } ,
+        1200:{
+            items:1
+        } 
+    }
+});
+$(document).ready(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
 // career pag logic 
 window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".navbar");
@@ -53,4 +150,11 @@ window.addEventListener("scroll", function() {
         navbar.classList.add("bg-nav");
 
     }
+});
+
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    document.querySelector('.background-left').style.transform = `translateY(${scrollPosition * 0.5}px)`;
+    document.querySelector('.background-right').style.transform = `translateY(${scrollPosition * -0.5}px)`;
 });
