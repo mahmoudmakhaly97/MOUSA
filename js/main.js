2b8b0e8c5b35d5446ce84fbb7708fecab3b007a6
mahmoudmakhaly97 <mahmoud.makhaly@5d-agency.com>
2024-06-09 23:51:06 +0200

    -add some enhancements

--- js/main.js
+++ js/main.js
@@ -143,12 +143,12 @@ $(document).ready(function(){
 window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".navbar");
     if (window.scrollY >= 100) {
-        navbar.classList.add("fixed-top");
+        navbar.classList.add("sticky-top" );
         navbar.classList.remove("bg-nav");
         
 
     } else {
-        navbar.classList.remove("fixed-top");
+        navbar.classList.remove("sticky-top");
         navbar.classList.add("bg-nav");
 
     }
